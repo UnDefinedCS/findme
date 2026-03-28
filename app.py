f536c7153513dd12ac75423bad8fcbd4a5875e49
nateBurnerAccount2026 <nateboothksu@gmail.com>
2026-03-28 16:30:18 +0000

    basic form retrieval

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def submit_info():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    social_media = request.form['social_media']
+    alias = request.form['alias']
+
+    print("First name: " + first_name, "Last name: " + last_name, "Online alias: " + alias, "Alias associated with: " + social_media)
+    
